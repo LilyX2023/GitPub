ed903347939a3c474771aff5ee2ddffd1cb73a33
RachelY <yang.yijing@outlook.com>
2023-12-17 00:06:53 -0500

    Welcome to gitpub

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Define a route for the root URL ("/")
 app.get('/', (req, res) => {
-  res.send('Hello, Express!');
+  res.send('Welcome to the Gitpub app');
 });
 
 // Start the server and listen on port 3000
